57094a495f331c36df3638409837344e3a69b6a8
Chris Williams <chrisfwilliams@gmail.com>
2023-07-15 20:32:23 -0400

    added comments to resource blocks

--- 01_multiple_providers_example/main.tf
+++ 01_multiple_providers_example/main.tf
@@ -4,20 +4,20 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-# Additional provider configuration; resources can
-# reference this as `aws.west`
+# Additional provider configuration; resources can reference this as `aws.west`
 provider "aws" {
   profile = "dev"
   region  = "us-west-2"
   alias   = "west"
 }
 
+# makes an EC2 instance in the default VPC of the account associated with the "prod" profile
 resource "aws_instance" "prod-ec2" {
     provider = aws.west
     ami = "ami-0507f77897697c4ba"
     instance_type = "t2.micro"
 }
-
+# makes an EC2 instance in the default VPC of the account associated with the "dev" profile
 resource "aws_instance" "dev-ec2" {
     ami = "ami-06ca3ca175f37dd66"
     instance_type = "t2.micro"
