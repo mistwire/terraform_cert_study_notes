eada1489803f3d10b9e69aa6624c5e7f4c0e7cd3
Chris Williams <chrisfwilliams@gmail.com>
2023-08-21 13:30:23 -0400

    re-screw-up fmt section

--- 02_fmt_example/main.tf
+++ 02_fmt_example/main.tf
@@ -1,7 +1,7 @@
 resource "random_string" "mistwire" {
-  length      = 16
-  special     = false
-  min_numeric = 10
-  min_special = 2
-  min_upper   = 3
+  length          =     16
+      special = false
+  min_numeric             =            10
+ min_special = 2
+             min_upper   = 3
 }
