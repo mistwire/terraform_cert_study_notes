56bbb6da9bc98842191a6d6de9f3c97a34228536
Chris Williams <chrisfwilliams@gmail.com>
2023-08-30 16:18:05 -0400

    add 06

---
+++ 06_Collections_examples/main.tf
@@ -0,0 +1,42 @@
+/*
+example of list(string)
+*/
+variable "public_subnet_cidr_blocks" {
+  description = "Available cidr blocks for public subnets."
+  type        = list(string)
+  default     = [
+    "10.0.1.0/24",
+    "10.0.2.0/24",
+    "10.0.3.0/24",
+    "10.0.4.0/24",
+    "10.0.5.0/24",
+    "10.0.6.0/24",
+    "10.0.7.0/24",
+    "10.0.8.0/24",
+  ]
+}
+
+/*
+example of map(string)
+*/ 
+variable "resource_tags" {
+  description = "Tags to set for all resources"
+  type        = map(string)
+  default     = {
+    project     = "project-alpha",
+    environment = "dev"
+  }
+}
+
+/*
+example of object({ name=string, age=number })
+*/ 
+{
+  name = "John"
+  age  = 52
+}
+
+/*
+example of tuple([string, number, bool])
+*/
+["a", 15, true]
